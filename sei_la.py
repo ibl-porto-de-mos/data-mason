a3b1a7c7a310b3c057f48b6b2277f036796d8cc2
Ester <estercalandrine323@gmail.com>
2025-11-08 09:41:14 +0000

    aula de hoje

---
+++ sei_la.py
@@ -0,0 +1,26 @@
+x: str = "8635372.98789"
+y: int = 8635372
+z: float = 8635372.98789
+
+minha_lista: list = [x,y,z]
+
+minha_lista.append(8967324875612378456)
+
+minha_tuple: tuple = (x,y,z)
+
+dicionario: dict = {
+    "key1": 100000,
+    "key2": "100000",
+    "key3": [],
+    "banasdjn": (1,2,3),
+    "adfoHNIPDBUF": dict()
+}
+
+print(type(minha_lista[0]))
+print(minha_tuple)
+
+def funcao(x):
+    return x + 2
+
+print(funcao(0))
+print(dicionario["key1"])
