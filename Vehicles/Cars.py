06e3303e939938f4f11c9c18f91c02783e0013e7
Maddu <mariaeduardacalandrine52@gmail.com>
2026-01-03 09:50:37 +0000

    feature

---
+++ Vehicles/Cars.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, engine, wheels, doors, chassis,fuel_tank):
+        self.Engine = engine
+        self.wheels = wheels
+        self.doors = doors
+        self.chassis = chassis
+        self.fuel_tank = fuel_tank
+
+    def accelerate (self):
+         "speed up!!!!"
+    
+    def stop (self):
+         "stopped"
