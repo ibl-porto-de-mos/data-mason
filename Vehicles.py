3ff350d38f2a931d87dd0142d97fdd72fc482483
dfjoao02 <dojoaosecundaria2@gmail.com>
2026-01-15 20:55:00 +0000

    Function changes and bug fixes

---
+++ Vehicles.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self, engine, wheels, doors, chassis, fuel_tank):
+
+
+        self.enine = enginen
+        self.wheels = wheels
+        self.doors = doors
+        self.chassis = chassis
+        self. fuel_tank = fuel_tank
+    
+    def accelerate(self):
+        return f("O motor {self.engine} ronca alto e o carro acelera com suas {self.wheels}!")
+    
+Volkswagen = Car("V8","rodas de liga leve", 4, "chassis", "80 litros")
+
+result = Volkswagen.accelerate()
+
+print(resultado)
+print(f"Tanque: {volkswagen.fuel_tank}")
