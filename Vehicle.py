f6d99af2a510aa6cdbc9e7d39da58fa8eeb548b6
dfjoao02 <dojoaosecundaria2@gmail.com>
2026-01-15 21:12:19 +0000

    (errado)
    Fix class initialization and method definition in Vehicle.py.

--- Vehicle.py
+++ Vehicle.py
@@ -1,18 +1,1 @@
-class Car:
-    def __init__(self,enginen, wheels, doors, chassis, fuel_tank):
 
-
-        self.eninen = enginen
-        self.wheels = wheels
-        self.doors= doors
-        self.chassis = chassis
-        self. fuel_tank = fuel_tank
-    
-    def acelerar (x,y):
-     return "Pão de Queijo"
-    
-Automóveis = Car ( "acelerar", "Parar", "construtor")
-
-Fabrica = Automóveis.acelerar("Carros são muito legais")
-
-print(Fabrica)
