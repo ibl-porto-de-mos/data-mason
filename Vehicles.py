75e0179931903b14894ee2de9d75d690db64b7d4
dfjoao02 <dojoaosecundaria2@gmail.com>
2026-01-15 21:39:26 +0000

    Changes in some important details for the system's operation

--- Vehicles.py
+++ Vehicles.py
@@ -2,18 +2,18 @@ class Car:
     def __init__(self, engine, wheels, doors, chassis, fuel_tank):
 
 
-        self.enine = enginen
+        self.engine = engine
         self.wheels = wheels
         self.doors = doors
         self.chassis = chassis
         self. fuel_tank = fuel_tank
     
     def accelerate(self):
-        return f("O motor {self.engine} ronca alto e o carro acelera com suas {self.wheels}!")
+        return f"O motor {self.engine} ronca alto e o carro acelera com suas {self.wheels}!"
     
-Volkswagen = Car("V8","rodas de liga leve", 4, "chassis", "80 litros")
+Volkswagen = Car("v8","rodas de liga leve", 4, "chassis", "80 litros")
 
 result = Volkswagen.accelerate()
 
 print(result)
-print(f"Tanque: {volkswagen.fuel_tank}")
+print(f"Tanque: {Volkswagen.fuel_tank}")
