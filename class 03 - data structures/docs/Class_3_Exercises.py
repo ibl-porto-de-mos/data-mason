0b9f902f714986cc32bda61b0f6c3e1eae5bcb4f
dfjoao02 <dojoaosecundaria2@gmail.com>
2026-02-26 22:29:05 +0000

    T.P.C Class 3

---
+++ class 03 - data structures/docs/Class_3_Exercises.py
@@ -0,0 +1,13 @@
+ # Exercises
+ 
+# 1. Create a list of parables and sort them.
+
+Parables = [ "The Good Samaritan", "The Prodige Son", "The Ten Virgins", " The Sower" ]
+Parables.append("The Lost Sheep")
+print(Parables[-2])
+
+
+#  2. Use a dict to map apostles to their roles.
+
+Apostles_roles = { "Peter": "Early church leader", "John": "Beloved Disciple", "Paul": "Missionary" }
+print(Apostles_roles["John"])
